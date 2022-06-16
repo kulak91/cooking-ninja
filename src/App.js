9d6d7832f43a4d4eb70495d69a0293f9f83f6a59
kulak91 <kulakovdmitr@gmail.com>
2022-06-16 03:05:06 +0300

    created pages skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+import Create from './pages/create/Create';
+import Navbar from './components/Navbar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     <BrowserRouter>
+    <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/create' element={<Create/>}/>
+      <Route path='/search' element={<Search/>}/>
+      <Route path='/recipes/:id' element={<Recipe/>}/>
       
-      </header>
+     </Routes>
+     </BrowserRouter>
+      
     </div>
   );
 }
